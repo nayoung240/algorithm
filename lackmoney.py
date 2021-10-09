f757cdfe539064103da3999e86f69f8ae1c5ea70
nayoung240 <iirony@naver.com>
2021-10-10 03:33:52 +0900

    Create lackmoney.py

---
+++ lackmoney.py
@@ -0,0 +1,23 @@
+'''
+이 놀이기구의 원래 이용료는 price원 인데, 
+놀이기구를 N 번 째 이용한다면 원래 이용료의 N배를 받기로 하였습니다. 
+즉, 처음 이용료가 100이었다면 2번째에는 200, 3번째에는 300으로 요금이 인상됩니다.
+놀이기구를 count번 타게 되면 현재 자신이 가지고 있는 금액에서 얼마가 모자라는지를 return 하도록 solution 함수를 완성하세요.
+단, 금액이 부족하지 않으면 0을 return 하세요.
+'''
+def solution(price, money, count):
+    pay = 0
+
+    for i in range(1, count + 1):
+        pay += price * i
+
+    answer = pay - money if money < pay else 0
+
+    return answer
+
+'''
+다른 사람 풀이
+for문 없이 작성
+'''
+def solution(price, money, count):
+    return max(price*count*(count+1) / 2 - money, 0)
