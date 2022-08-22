f79d0d0112c3a336ee45ffa3a615474ba1934f80
nayoung240 <iirony@naver.com>
2022-08-22 21:54:19 +0900

    Update targetnumber.py

--- targetnumber.py
+++ targetnumber.py
@@ -1,3 +1,10 @@
+'''
+사용할 수 있는 숫자가 담긴 배열 numbers, 타겟 넘버 target이 매개변수로 주어질 때 숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수를 return 하도록 solution 함수를 작성해주세요.
+
+numbers	        target	return
+[1, 1, 1, 1, 1]	3	    5
+[4, 1, 2, 1]	4	    2
+'''
 count = 0
 
 def dfs(idx, value, numbers, target):
