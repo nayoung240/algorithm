8a67903a95741a09763d5ce3a50555817a46cbae
nayoung240 <iirony@naver.com>
2022-10-16 21:47:42 +0900

    Update biggestnumber.py

--- biggestnumber.py
+++ biggestnumber.py
@@ -1,5 +1,12 @@
 '''
 permutations 순열로 풀었을 때, 정답은 맞지만 모든 케이스를 만드므로 시간초과 발생
+
+풀이포인트
+sort key lambda
+key인자에 함수를 넘겨주면 우선순위가 정해진다
+d = sorted(a, key = lambda x : x[1]) 
+d = [(3, 0), (5, 1), (0, 1), (1, 2), (5, 2)]
+
 '''
 def solution(numbers):
     #1. 사전 값으로 정렬하기
