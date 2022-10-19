4a0b090ac374cad484d19cc1fe19cfc34680b48d
nayoung240 <iirony@naver.com>
2022-10-19 15:06:05 +0900

    Create dfsbfs.py

---
+++ dfsbfs.py
@@ -0,0 +1,60 @@
+'''
+양방향 그래프
+ex)
+4 5 1
+1 2
+1 3
+1 4
+2 4
+3 4
+'''
+from collections import deque
+
+def DFS(graph, root):
+    visited = []
+    stack = [root]
+
+    while stack:
+        n = stack.pop()
+        if n not in visited:
+            visited.append(n)
+            if n in graph:
+                temp = list(set(graph[n]) - set(visited))
+                temp.sort(reverse=True)
+                stack += temp
+    return " ".join(str(i) for i in visited)
+
+def BFS(graph, root):
+    visited = []
+    queue = deque([root])
+
+    while queue:
+        n = queue.popleft()
+        if n not in visited:
+            visited.append(n)
+            if n in graph:
+                temp = list(set(graph[n]) - set(visited))
+                temp.sort()
+                queue += temp
+    return " ".join(str(i) for i in visited)
+
+  
+graph = {}
+n = input().split(' ')
+node, edge, start = [int(i) for i in n]
+for i in range(edge):
+    edge_info = input().split(' ')
+    n1, n2 = [int(j) for j in edge_info]
+    if n1 not in graph:
+        graph[n1] = [n2]
+    elif n2 not in graph[n1]:
+        graph[n1].append(n2)
+
+    if n2 not in graph:
+        graph[n2] = [n1]
+    elif n1 not in graph[n2]:
+        graph[n2].append(n1)
+
+# graph {1:[2,3,4], 2:[1,4], 3:[1,4], 4:[1,2,3]}
+print(DFS(graph, start))
+print(BFS(graph, start))
