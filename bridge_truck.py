4616507c2c8d8e5a557ccebb52d348e215167c2b
nayoung240 <iirony@naver.com>
2022-12-11 22:49:25 +0900

    Update bridge_truck.py

--- bridge_truck.py
+++ bridge_truck.py
@@ -2,18 +2,20 @@ from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
     answer = 0
-    truck_bridge_dq = deque(bridge_length * [0]) # 다리 단위 만큼
-    truck_weights_dq = deque(truck_weights)
+    temp = 0 #다리위 트럭 무게 합
+    truck_bridge_deque = deque(bridge_length * [0]) #다리 단위 만큼
+    truck_weights_deque = deque(truck_weights)
     
-    while len(truck_bridge_dq):
-        print(truck_bridge_dq)
+    while len(truck_bridge_deque):
         answer += 1
-        truck_bridge_dq.popleft()
+        temp -= truck_bridge_deque[0]
+        truck_bridge_deque.popleft()
         
-        if truck_weights_dq:
-            # 다리위에 더 트럭 추가 가능할 경우
-            if sum(truck_bridge_dq) + truck_weights_dq[0] <= weight:
-                truck_bridge_dq.append(truck_weights_dq.popleft())
+        if truck_weights_deque:
+            # 다리위에 트럭이 더 올라갈 수 있으면
+            if temp + truck_weights_deque[0] <= weight:
+                temp += truck_weights_deque[0]
+                truck_bridge_deque.append(truck_weights_deque.popleft())
             else:
-                truck_bridge_dq.append(0)
+                truck_bridge_deque.append(0)
     return answer
