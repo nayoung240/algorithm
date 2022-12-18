151ff5df21296f0aaeaf5da759e2f10491be5e09
nayoung240 <iirony@naver.com>
2022-12-18 23:24:39 +0900

    Update circlesum.py

--- circlesum.py
+++ circlesum.py
@@ -11,3 +11,18 @@ def solution(elements):
             res.add(ssum)
             
     return len(res)
+
+'''
+두배로 늘림
+'''
+def solution(elements):
+    result = set()
+    
+    elementLen = len(elements)
+    # [7, 9, 1, 1, 4, 7, 9, 1, 1, 4]
+    elements = elements * 2
+    
+    for i in range(elementLen):
+        for j in range(elementLen):
+            result.add(sum(elements[j:j+i+1]))
+    return len(result)
