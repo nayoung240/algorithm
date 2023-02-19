a88718d45a38a320dc2979647db8bbf1369baa31
nayoung240 <iirony@naver.com>
2023-02-19 22:58:48 +0900

    Update assigncookies.py

--- greedy/assigncookies.py
+++ greedy/assigncookies.py
@@ -3,7 +3,28 @@
 g = [1,2,3]
 s = [3]
 1이 나와야한다.
+'''
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        count = 0
+        g.sort()
+        s.sort(reverse=True)
 
+        for ss in s:
+            if not g:
+                break
+
+            while g:
+                gg = g.pop()
+
+                if ss >= gg:
+                    count += 1
+                    break
+                
+
+        return count
+    
+'''
 내림차순으로 풀어야하나 했는데 오름차순으로 풀면된다.
 '''
 class Solution:
