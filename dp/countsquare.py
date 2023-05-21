4d8ae720ca27e74198f039e34876038d96d3ea06
nayoung240 <iirony@naver.com>
2023-05-21 21:04:13 +0900

    Update and rename countSquare.py to countsquare.py

--- dp/countsquare.py
+++ dp/countsquare.py
@@ -7,10 +7,8 @@ class Solution:
 
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                print(i, j)
                 if matrix[i][j] == 1 and (i != 0 and j != 0):
                     matrix[i][j] = min(matrix[i-1][j-1], matrix[i-1][j], matrix[i][j-1]) + 1
-                    print(matrix)
 
             count += sum(matrix[i])
 
