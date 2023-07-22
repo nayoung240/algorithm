85e39cd24fddecb202cf7715894e93f69cde1e4d
nayoung240 <iirony@naver.com>
2023-07-23 01:11:06 +0900

    Update countrectangles.py

--- binary/countrectangles.py
+++ binary/countrectangles.py
@@ -3,10 +3,21 @@ class Solution:
     Return 포인트를 포함하고 있는 직사각형의 수
     '''
     def countRectangles(self, rectangles: List[List[int]], points: List[List[int]]) -> List[int]:
+        answer = []
         rectangles.sort()
         length = defaultdict(list)
-        
+
         for l, h in rectangles:
             length[h].append(l)
+
+        for x, y in points:
+            a = []
+
+            for h in range(y, 101):
+                b = len(length[h]) - bisect.bisect_left(length[h], x)
+                a.append(b)
+                
+            answer.append(sum(a))
+
+        return answer
         
-        return [sum(len(length[h]) - bisect.bisect_left(length[h], x) for h in range(y, 101)) for x, y in points]
